953d84dac8e2eaffa28de44195aaf07f4b9c6ecd
blackfeather289 <m22cs058@iitj.ac.in>
2022-09-16 17:06:32 +0530

    Used different image sizes

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -6,9 +6,10 @@
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
+from skimage import transform
 from sklearn.model_selection import train_test_split
 
 # 1. set the ranges of hyper parameters 
@@ -25,7 +26,6 @@ dev_frac = 0.1
 
 #PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
-
 #PART: sanity check visualization of the data
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
@@ -33,83 +33,89 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
 
-
+print(f"Image size of this dataset is {digits.images[0].shape}")
 #PART: data pre-processing -- to remove some noise, to normalize data, format the data to be consumed by mode
 # flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+images = {}
+def resize_and_predict(size = 8):
+    print(f"\n----------FOR IMAGE SIZE ({size},{size})-----------\n")
+    n_samples = len(digits.images)
+    images[str(size)] = np.zeros((n_samples,size, size))
+    for i in range(0,n_samples):
+        images[str(size)][i] = transform.resize(digits.images[i],(size, size),anti_aliasing=True)
+    data = images[str(size)].reshape((n_samples, -1))
 
 
-#PART: define train/dev/test splits of experiment protocol
-# train to train model
-# dev to set hyperparameters of the model
-# test to evaluate the performance of the model
-dev_test_frac = 1-train_frac
-X_train, X_dev_test, y_train, y_dev_test = train_test_split(
-    data, digits.target, test_size=dev_test_frac, shuffle=True
-)
-X_test, X_dev, y_test, y_dev = train_test_split(
-    X_dev_test, y_dev_test, test_size=(dev_frac)/dev_test_frac, shuffle=True
-)
+    #PART: define train/dev/test splits of experiment protocol
+    # train to train model
+    # dev to set hyperparameters of the model
+    # test to evaluate the performance of the model
+    dev_test_frac = 1-train_frac
+    X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+        data, digits.target, test_size=dev_test_frac, shuffle=True
+    )
+    X_test, X_dev, y_test, y_dev = train_test_split(
+        X_dev_test, y_dev_test, test_size=(dev_frac)/dev_test_frac, shuffle=True
+    )
 
 
-best_acc = -1.0
-best_model = None
-best_h_params = None
+    best_acc = -1.0
+    best_model = None
+    best_h_params = None
 
-# 2. For every combination-of-hyper-parameter values
-for cur_h_params in h_param_comb:
+    # 2. For every combination-of-hyper-parameter values
+    for cur_h_params in h_param_comb:
 
-    #PART: Define the model
-    # Create a classifier: a support vector classifier
-    clf = svm.SVC()
+        #PART: Define the model
+        # Create a classifier: a support vector classifier
+        clf = svm.SVC()
 
-    #PART: setting up hyperparameter
-    hyper_params = cur_h_params
-    clf.set_params(**hyper_params)
-
-
-    #PART: Train model
-    # 2.a train the model 
-    # Learn the digits on the train subset
-    clf.fit(X_train, y_train)
-
-    # print(cur_h_params)
-    #PART: get dev set predictions
-    predicted_dev = clf.predict(X_dev)
-
-    # 2.b compute the accuracy on the validation set
-    cur_acc = metrics.accuracy_score(y_pred=predicted_dev, y_true=y_dev)
-
-    # 3. identify the combination-of-hyper-parameter for which validation set accuracy is the highest. 
-    if cur_acc > best_acc:
-        best_acc = cur_acc
-        best_model = clf
-        best_h_params = cur_h_params
-        print("Found new best acc with :"+str(cur_h_params))
-        print("New best val accuracy:" + str(cur_acc))
+        #PART: setting up hyperparameter
+        hyper_params = cur_h_params
+        clf.set_params(**hyper_params)
 
 
+        #PART: Train model
+        # 2.a train the model 
+        # Learn the digits on the train subset
+        clf.fit(X_train, y_train)
 
-    
-#PART: Get test set predictions
-# Predict the value of the digit on the test subset
-predicted = best_model.predict(X_test)
+        # print(cur_h_params)
+        #PART: get dev set predictions
+        predicted_dev = clf.predict(X_dev)
 
-#PART: Sanity check of predictions
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+        # 2.b compute the accuracy on the validation set
+        cur_acc = metrics.accuracy_score(y_pred=predicted_dev, y_true=y_dev)
 
-# 4. report the test set accurancy with that best model.
-#PART: Compute evaluation metrics
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+        # 3. identify the combination-of-hyper-parameter for which validation set accuracy is the highest. 
+        if cur_acc > best_acc:
+            best_acc = cur_acc
+            best_model = clf
+            best_h_params = cur_h_params
+            print("Found new best acc with :"+str(cur_h_params))
+            print("New best val accuracy:" + str(cur_acc))
 
-print("Best hyperparameters were:")
-print(cur_h_params)
+
+
+        
+    #PART: Get test set predictions
+    # Predict the value of the digit on the test subset
+    predicted = best_model.predict(X_test)
+
+    #PART: Sanity check of predictions
+
+
+    # 4. report the test set accurancy with that best model.
+    #PART: Compute evaluation metrics
+    print(
+        f"Classification report for classifier {clf}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+
+    print("Best hyperparameters were:")
+    print(cur_h_params)
+
+resize_and_predict()
+resize_and_predict(4)
+resize_and_predict(16)
+resize_and_predict(32)
