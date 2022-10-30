e38d3dc476cb5804a3afe20de54cf6e01d8428b2
M22CS058 <m22cs058@iitj.ac.in>
2022-10-30 17:46:48 +0530

    Added label comparison

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,14 +1,8 @@
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-
-# PART: library dependencies -- sklear, torch, tensorflow, numpy, transformers
-
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 import pdb
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+import pandas as pd
 
 from utils import (
     preprocess_digits,
@@ -24,7 +18,6 @@ from joblib import dump, load
 train_frac, dev_frac, test_frac = 0.8, 0.1, 0.1
 assert train_frac + dev_frac + test_frac == 1.0
 
-# 1. set the ranges of hyper parameters
 gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
 c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
 
@@ -33,7 +26,7 @@ params["gamma"] = gamma_list
 params["C"] = c_list
 
 h_param_comb = get_all_h_param_comb(params)
-
+max_depth_list = [5, 10, 20, 40, 80, 160]
 
 # PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
@@ -46,6 +39,7 @@ n_cv = 5
 results = {}
 random = [14, 56, 290, 456, 78]
 metric = metrics.accuracy_score
+predicted = {}
 for i in range(n_cv):
 
     x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(
@@ -56,29 +50,23 @@ for i in range(n_cv):
     # Create a classifier: a support vector classifier
     models_of_choice = {'svm':svm.SVC(), 'decision_tree':DecisionTreeClassifier()}
     for clf_name in models_of_choice:
-    # define the evaluation metric
         best_model = models_of_choice[clf_name]
-        '''actual_model_path = tune_and_save(
-            clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb[clf_name], model_path=None
-        )'''
-
-
+        
         # 2. load the best_model
         #best_model = load(actual_model_path)
         best_model.fit(x_train, y_train)
-        # PART: Get test set predictions
-        # Predict the value of the digit on the test subset
-        predicted = best_model.predict(x_test)
+        
+        predicted[clf_name] = best_model.predict(x_test)
         if not clf_name in results:
             results[clf_name] = []
-        results[clf_name].append(metric(y_pred = predicted, y_true = y_test))
+        results[clf_name].append(metric(y_pred = predicted[clf_name], y_true = y_test))
 
-        # 4. report the test set accurancy with that best model.
-        # PART: Compute evaluation metrics
-        '''print(
-            f"Classification report for classifier {best_model}:\n"
-            f"{metrics.classification_report(y_test, predicted)}\n"
-        )'''
+
+        
 print(results)
 print("Mean and standard deviation for svm are {:.4} and {:.4}".format(np.mean(results['svm']), np.std(results['svm'])))
 print("Mean and standard deviation for decision_tree are {:.4} and {:.4}".format(np.mean(results['decision_tree']), np.std(results['decision_tree'])))
+print("Actual Vs Predicted Labels using SVM:")
+print(list(zip(list(y_test), list(predicted['svm']))))
+print("Actual Vs Predicted Labels using Decision Tree:")
+print(list(zip(list(y_test), list(predicted['decision_tree']))))
