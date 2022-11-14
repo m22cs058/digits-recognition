869db44ea958e559b78adbb4966081b9716a3d91
M22CS058 <m22cs058@iitj.ac.in>
2022-11-14 09:58:20 +0530

    Image prediction API

---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from joblib import load
+
+app = Flask(__name__)
+model_path = "svm_gamma=0.001_C=0.5.joblib"
+model = load(model_path)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/sum", methods = ['POST'])
+def sum():
+    print(request.json)
+    x = request.json['x']
+    y = request.json['y']
+    z = x + y
+    return jsonify({'sum' : z})
+
+@app.route("/predict", methods = ["POST"])
+def predict_class():
+    image = request.json['image']
+    print("Loading successful")
+    predicted = model.predict([image])
+    return {"y_predicted" : int(predicted[0])}
