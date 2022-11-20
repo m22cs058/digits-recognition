995327f41ca032e0e9b1ed40d26dbfd40182d26b
M22CS058 <m22cs058@iitj.ac.in>
2022-11-20 19:53:25 +0530

    Q2

---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+import argparse
+from joblib import dump
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+digits = datasets.load_digits()
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+parser = argparse.ArgumentParser(description ='Model Training')
+parser.add_argument('--clf_name', type = str)
+
+parser.add_argument('--random_state', type = int)
+args = parser.parse_args()
+
+clf_name = args.clf_name
+random_state = args.random_state
+
+
+
+models = {"svm": svm.SVC(gamma=0.001), 'decision_tree':DecisionTreeClassifier()}
+
+
+def train(clf_name, random_state):
+    X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, random_state = random_state
+)
+    clf = models[clf_name]
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test)
+    print("test accuracy: " + str(accuracy_score(predicted, y_test)))
+    print("test macro-f1: " + str(f1_score(predicted.reshape(-1,1), y_test.reshape(-1,1), average = "macro")))
+    dump(clf, clf_name + "_" + str(random_state) + ".joblib")
+
+train(clf_name, random_state)
+
