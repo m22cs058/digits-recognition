3b7301afdb4100674c54d378b96e248a7d4d0edc
M22CS058 <m22cs058@iitj.ac.in>
2022-11-20 21:32:53 +0530

    Final

--- api/app.py
+++ api/app.py
@@ -4,8 +4,8 @@ from flask import jsonify
 from joblib import load
 
 app = Flask(__name__)
-#model_path = "models/svm_random_state=84.joblib"
-#model = load(model_path)
+model_path = "models/svm_random_state=84.joblib"
+model = load(model_path)
 
 @app.route("/predict", methods = ["POST"])
 def predict_class():
@@ -13,7 +13,8 @@ def predict_class():
     model_path = request.json['model']
     model = load(model_path)
     predicted1 = model.predict([image1])
-    return predicted1
+    print(predicted1)
+    return int(predicted1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
