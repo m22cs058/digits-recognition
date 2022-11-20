5e7d3adf80c9c13d3f462717f0a50527996e774b
M22CS058 <m22cs058@iitj.ac.in>
2022-11-20 21:05:12 +0530

    Q4

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from joblib import load
+
+app = Flask(__name__)
+#model_path = "models/svm_random_state=84.joblib"
+#model = load(model_path)
+
+@app.route("/predict", methods = ["POST"])
+def predict_class():
+    image1 = request.json['image1']
+    model_path = request.json['model']
+    model = load(model_path)
+    predicted1 = model.predict([image1])
+    return predicted1
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
